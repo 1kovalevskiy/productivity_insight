82a54865ec616cf2dffe707c71055b3c093797ab
Nikita Kovalevskiy <koval701@gmail.com>
2022-07-06 22:08:36 +0700

    create releationship between users and recipe in favorite and like

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,11 +1,22 @@
 from datetime import date
 
 from sqlalchemy import Column, ForeignKey, Date, Text, Boolean, Integer
+from sqlalchemy.orm import relationship
 
 from app.core.db import Base
 
 
-class Recipe (Base):
+class Like(Base):
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipe.id'), primary_key=True)
+
+
+class Favorite(Base):
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipe.id'), primary_key=True)
+
+
+class Recipe(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     create = Column(Date, default=date.today, nullable=False)
     update = Column(Date, default=date.today, nullable=False)
@@ -15,3 +26,5 @@ class Recipe (Base):
     steps = Column(Text, nullable=False)
     photo = Column(Text, nullable=False)
     status = Column(Boolean, default=1)
+    in_favorite = relationship("User", secondary="favorite", back_populates='favorite')
+    in_like = relationship("User", secondary="like", back_populates='like')
--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import Column, String, Date
+from sqlalchemy.orm import relationship
 
 from app.core.db import Base
 
@@ -10,3 +11,5 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     nickname = Column(String, nullable=False)
     create = Column(Date, default=date.today, nullable=False)
     update = Column(Date, default=date.today, nullable=False)
+    favorite = relationship("Recipe", secondary="favorite", back_populates='in_favorite')
+    like = relationship("Recipe", secondary="like", back_populates='in_like')
